35eb3394611167195be56e1dbc82728bd1285152
AalguienN <93289662+AalguienN@users.noreply.github.com>
2025-04-09 18:31:49 +0200

    Agente básico

---
+++ Assets/Scripts/MLAgents/PlayerAgent.cs
@@ -0,0 +1,20 @@
+using Unity.MLAgents;
+using UnityEngine;
+
+namespace Undercooked
+{
+    public class PlayerAgent : Agent
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
